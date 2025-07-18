f62445be1dcd0bcdd17067fbb0a385b135a59af7
Antoine Chaing <antoine.chaing@epita.fr>
2025-07-18 23:14:05 +0200

    fix(recovery_projectile_handler): Adding scene for explosion

--- scripts/projectile_component.gd
+++ scripts/projectile_component.gd
@@ -3,6 +3,7 @@ class_name ProjectileComponent
 
 @export var SPEED = 500
 @export var DAMAGE = 20
+@export var explosion_particle_scene: PackedScene
 
 @onready var parent = get_parent()
 
@@ -23,26 +24,33 @@ func create_explosion_gradient() -> Gradient:
 	return gradient
 
 func destroy_projectile(hit_position: Vector2):
-	var particles = CPUParticles2D.new()
 	parent.cleanse()
-		
-	parent.add_child(particles)
-	particles.global_position = hit_position
 	
-	particles.emitting = true
-	particles.amount = 30
-	particles.lifetime = 0.5
-	particles.emission_shape = CPUParticles2D.EMISSION_SHAPE_SPHERE
-	particles.emission_sphere_radius = 5.0
-	particles.direction = Vector2(1, 0)
-	particles.spread = 360.0
-	particles.initial_velocity_min = 180.0
-	particles.initial_velocity_max = 200.0
-	particles.scale_amount_min = 2.8
-	particles.scale_amount_max = 4.0
-	particles.color = Color(1.0, 0.6, 0.1, 1.0)
-	particles.gravity = Vector2(0, 0)
-	particles.linear_accel_min = -10.0
-	particles.linear_accel_max = -5.0
-	particles.color_ramp = create_explosion_gradient()
-	particles.one_shot = true
+	if explosion_particle_scene:
+		var explosion_instance = explosion_particle_scene.instantiate()
+		parent.add_child(explosion_instance)
+		explosion_instance.global_position = hit_position
+		var particles = explosion_instance.get_node("ExplosionElement") as CPUParticles2D
+		if particles:
+			particles.emitting = true
+	
+	#parent.add_child(particles)
+	#particles.global_position = hit_position
+	#
+	#particles.emitting = true
+	#particles.amount = 30
+	#particles.lifetime = 0.5
+	#particles.emission_shape = CPUParticles2D.EMISSION_SHAPE_SPHERE
+	#particles.emission_sphere_radius = 5.0
+	#particles.direction = Vector2(1, 0)
+	#particles.spread = 360.0
+	#particles.initial_velocity_min = 180.0
+	#particles.initial_velocity_max = 200.0
+	#particles.scale_amount_min = 2.8
+	#particles.scale_amount_max = 4.0
+	#particles.color = Color(1.0, 0.6, 0.1, 1.0)
+	#particles.gravity = Vector2(0, 0)
+	#particles.linear_accel_min = -10.0
+	#particles.linear_accel_max = -5.0
+	#particles.color_ramp = create_explosion_gradient()
+	#particles.one_shot = true
